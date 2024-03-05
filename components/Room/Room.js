23e6181111393bf42c8f6a5a4882c77b33e07f8d
madhurima5978 <madhurimadarivemula03@gmail.com>
2024-03-06 00:36:00 +0530

    basic ui finished

--- components/Room/Room.js
+++ components/Room/Room.js
@@ -1,17 +1,76 @@
-import { View, Text,StyleSheet } from 'react-native'
-import React from 'react'
-import Stories from '../Event/Stories'
-const Room = () => {
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Modal, Button, TouchableWithoutFeedback, Platform } from 'react-native';
+import { USERS } from '../../data/Users';
+
+const Stories = () => {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
-    <View style={styles.container }>
-        <Text style={{fontWeight:'800', fontSize:20}}>Let's Talk</Text>
-      <Stories/>
+    <View style={styles.container}>
+      {USERS.map((userObj, index) => (
+        <TouchableOpacity key={userObj.user} style={styles.userContainer} onPress={toggleModal}>
+          <Image
+            source={{ uri: userObj.image }}
+            style={{ width: 100, height: 100, borderRadius: 50 }}
+          />
+          <Text>{userObj.user}</Text>
+        </TouchableOpacity>
+      ))}
+
+      <Modal animationType="slide" transparent={true} visible={isModalVisible}>
+        <TouchableWithoutFeedback onPress={closeModal}>
+          <View style={styles.overlay} />
+        </TouchableWithoutFeedback>
+        <View style={styles.modalContainer}>
+          <Text>Join Room</Text>
+          <Button title="Close" onPress={closeModal} />
+        </View>
+      </Modal>
     </View>
-  )
-}
+  );
+};
+
 const styles = StyleSheet.create({
-    container:{
-        padding: 10
-    }
-})
-export default Room
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between', // Adjust as needed
+    paddingHorizontal: 10, // Adjust as needed
+  },
+  userContainer: {
+    alignItems: 'center',
+    marginVertical: 10, // Adjust as needed
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContainer: {
+    height: '50%', // Adjust as needed
+    backgroundColor: 'white',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 20,
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});
+
+export default Stories;
